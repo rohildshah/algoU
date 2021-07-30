026c05c32b1b49069d666dbabb0be3a20061c99d
rohildshah <shah5963@gmail.com>
2021-07-30 07:53:36 -0700

    Fix build script copy command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -R dist ~/Sites/dist
-cp -R public ~/Sites/public
+cp -R dist ~/Sites/
+cp -R public ~/Sites/
 cp index.html ~/Sites/index.html
 echo Copied to ~/Sites directory successfully!
