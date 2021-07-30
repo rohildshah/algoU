dac9f1d56aa26c74988a11ca09ae9e0daf9d2930
rohildshah <shah5963@gmail.com>
2021-07-30 08:11:39 -0700

    Crude database plumbing working

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -5,8 +5,10 @@ class CodeEditor extends React.Component {
     constructor() {
         super();
         this.onChange = this.onChange.bind(this)
+        this.onChangeLevel = this.onChangeLevel.bind(this)
         this.state = {
-            value: "from SortableList import SortableList\n\ndef bubbleSort(arr):\n    \n\narr = SortableList([64, 34, 25, 12, 22, 11, 90])\n\nbubbleSort(arr)\n\nfor i in range(len(arr.data)):\n    print('% d,' % arr.data[i])\n\nprint(arr.swaps)"
+            value: "from SortableList import SortableList\n\ndef bubbleSort(arr):\n    \n\narr = SortableList([64, 34, 25, 12, 22, 11, 90])\n\nbubbleSort(arr)\n\nfor i in range(len(arr.data)):\n    print('% d,' % arr.data[i])\n\nprint(arr.swaps)",
+            level: 1
         }
     }
 
@@ -16,7 +18,19 @@ class CodeEditor extends React.Component {
         })
     }
 
+    onChangeLevel(newValue) {
+        this.setState({
+            level: newValue
+        })
+    }
+
     render() {
+        const options = [
+            { value: 1, label: 'Level 1: Sorting' },
+            { value: 2, label: 'Level 2: placeholder' },
+            { value: 3, label: 'Level 3: placeholder' }
+        ]
+        
         return (
             <div className="w-50 h-100">
                 <AceEditor
@@ -41,7 +55,28 @@ class CodeEditor extends React.Component {
                     .catch((error) => {
                         console.log('Error:', error);
                     });
-                }} className="w-100 h-25">Send!</button>
+                }} className="">Send!</button>
+                {/* w-100 h-25 */}
+
+                <div onClick={() => {
+                    this.onChangeLevel(this.state.level != 3 ? this.state.level + 1 : 1)
+                }}>{this.state.level}</div>
+
+                <button type="button" onClick={() => {
+                    const data = { level: this.state.level }
+                    fetch('http://192.168.1.72:5000', {
+                        method: 'POST',
+                        body: JSON.stringify(data)
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('Success:', data);
+                        this.onChange(data['result'])
+                    })
+                    .catch((error) => {
+                        console.log('Error:', error);
+                    });
+                }}>Refresh!</button>
             </div>
         )
     }
